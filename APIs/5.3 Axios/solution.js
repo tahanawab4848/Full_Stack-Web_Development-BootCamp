1b4fef0dc8044522315a7be395dfe1dbbb0ab478
Muhammad Taha Nawab <tisart4848@gmail.com>
2026-06-09 13:13:23 +0500

    Change server port from 3000 to 5000

--- APIs/5.3 Axios/solution.js
+++ APIs/5.3 Axios/solution.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
